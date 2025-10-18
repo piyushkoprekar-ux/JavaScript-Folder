3c2273c09bdc0e054c9658417fb95b72dae6acbf
piyush <piyushkoprekar@gmail.com>
2025-10-18 20:43:39 +0530

    new folder

---
+++ Number/number.js
@@ -0,0 +1,44 @@
+//tostring
+let x = 124;
+let y = x.toString();
+console.log(y);
+
+//toexpontial
+var a = 1.654;
+var b = a.toExponential(4);
+console.log(b);
+
+//tofixed
+let text = 9.432;
+let c = text.toFixed(5);
+console.log(c);
+
+//toprecision
+let e = 4.726;
+let f = e.toPrecision(6);
+console.log(f);
+
+//valueof
+let d = 786;
+let g = d.valueOf();
+console.log(g);
+
+//isfinite
+let p = 345;
+let q = Number.isFinite(p);
+console.log(q);
+
+//Number
+var z = "99";
+var num = Number(z);
+document.write(z+10);
+
+//parselent
+var u = "99 88";
+var num = parseInt(u);
+document.writeln(num);
+
+//paresfloat
+var i = "10.33";
+var num = parseFloat(i);
+document.write(num);
