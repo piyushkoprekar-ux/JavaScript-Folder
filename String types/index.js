d5224eada23e1a826614e29e0f4674de612e1f80
piyush <piyushkoprekar@gmail.com>
2025-10-18 20:56:38 +0530

    New file

---
+++ String types/index.js
@@ -0,0 +1,38 @@
+// text.length
+let str = "Piyush";
+console.log(str.length); // Output: 5
+
+//charcodeAt
+let text = "P";
+console.log(text.charCodeAt(0)); // Output: 65
+
+//charAt
+let num = "Hello";
+console.log(num.charAt(1)); // Output: "e"
+
+//concat
+let str1 = "Piyush";
+let str2 = "Koprekar";
+console.log(str1.concat(" ", str2)); // Output: "Piyush Koprekar"
+
+//Slice
+let str3 = "Tonny stark";
+console.log(str3.slice(0, 5)); // Output: "Tonny"
+
+//substring
+let str4 = "Iron Man";
+console.log(str4.substring(0, 5)); // Output: "Iron"
+
+//substr
+let str5 = "Captain America";
+console.log(str5.substr(7, 9)); // Output: "America"
+
+//touppercase
+let str6 = "tanjiro";
+console.log(str6.toUpperCase()); // Output: "TANJIRO"
+
+//tolowercase
+let str7 = "NEZUKO";
+console.log(str7.toLowerCase()); // Output: "nezuko"
+
+
